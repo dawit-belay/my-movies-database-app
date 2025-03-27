444b8b68b91df6c37b35842689e8580f9a2bae41
dawit-belay <76816055+dawit-belay@users.noreply.github.com>
2025-03-27 17:32:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import React from 'react'
 import Search from './components/Search'
-import Spinner from './components/spinner';
+import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
 import { updateSearchCount } from './appwrite';
